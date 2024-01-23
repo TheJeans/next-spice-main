5a6cef519f6d0bce723f959885c63478e5bf77c4
Darian Glausier <48654987+TheJeans@users.noreply.github.com>
2024-01-23 14:37:57 -0500

    run 1 - home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,34 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="p-24">
-      <div>Interview Spice</div>
-      <div>
-        <div>
-          <Link href="/spices">Spices</Link>
-        </div>
-        <div>
-          <Link href="/blends">Blends</Link>
-        </div>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <a href="#mainContent" className="skip-link">
+                Skip to content
+            </a>
+
+            <header>
+                <h1>Interview Spice</h1>
+            </header>
+
+            <nav aria-label="Main navigation">
+                <ul>
+                    <li>
+                        <Link href="/spices" aria-label="Learn more about spices">
+                            Spices
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blends" aria-label="Learn more about blends">
+                            Blends
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <main id="mainContent">
+                {/* something here later */}
+            </main>
+        </>
+    );
 }
