df14efb2704418c5fae1c3dd215d5b28d4a87e81
Darian Glausier <48654987+TheJeans@users.noreply.github.com>
2024-01-25 16:55:42 -0500

    Update SplitSectionWithLinks.tsx

--- components/SplitSectionWithLinks.tsx
+++ components/SplitSectionWithLinks.tsx
@@ -7,7 +7,7 @@ function SplitSectionWithLinks({}: Props) {
     return (
         <section className="container mx-auto px-4 py-10">
             <div className="flex flex-wrap -m-4">
-                <div className="w-full md:w-1/2 p-4 border-r border-gray-300">
+                <div className="w-full md:w-1/2 p-4 border-b border-r-0 md:border-b-0 md:border-r border-gray-300">
                     <div className="py-32 px-8 text-center h-full">
                         <Link href={"/spices"} className="text-2xl block">
                             Learn more about
